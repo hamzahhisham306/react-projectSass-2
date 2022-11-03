b0b45cea417ffd8740ed559faac6e9eb4cf598d0
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-11-03 14:57:20 +0000

    v2

--- react-sass/src/App.js
+++ react-sass/src/App.js
@@ -3,11 +3,15 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Main from './components/main';
+import About from './components/About';
+import Articles from './components/Articles';
 function App() {
   return (
     <Container >
      <Header/>
      <Main/>
+     <About/>
+     <Articles/>
     </Container>
   );
 }
